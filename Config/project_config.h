b6d57b4df947a78e930e0e68935066266afba171
shohanur00 <shohanur.waltonbd@gmail.com>
2026-09-07 17:46:56 +0600

    first commit

---
+++ Config/project_config.h
@@ -0,0 +1,11 @@
+#ifndef PROJECT_CONFIG_H
+#define PROJECT_CONFIG_H
+
+#define MCU_FAMILY_STM32G431
+#define PROJECT_VERSION_MAJOR       1
+#define PROJECT_VERSION_MINOR       0
+
+#define ENABLE_DEBUG                1
+#define ENABLE_FAULT_HANDLER        1
+
+#endif
